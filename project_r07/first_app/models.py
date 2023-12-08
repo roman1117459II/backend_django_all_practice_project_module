f932306380ba3cb765e7d00b41da6fa8d5933714
roman1117459II <roman1117459@gmail.com>
2023-12-09 02:52:41 +0600

    update done for practice

--- project_r07/first_app/models.py
+++ project_r07/first_app/models.py
@@ -7,6 +7,14 @@ class StudentModel(models.Model):
     name = models.CharField(max_length= 20)
     father_name = models.CharField(max_length=30)
     address = models.TextField()
-    
+    date_field = models.DateField()
+    decimal_field = models.DecimalField(max_digits=5, decimal_places=2)
+    # file_path_field = models.FilePathField(path='/path/to/files/')
+    generic_ip_address_field = models.GenericIPAddressField()
+    json_field = models.JSONField()
+    slug_field = models.SlugField()
+    uuid_field = models.UUIDField()
+
+
     def __str__(self):
         return f"Name: {self.name}"
