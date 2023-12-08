db1b97cbd36c225a05bb171116304f97c15a7dd8
roman1117459II <roman1117459@gmail.com>
2023-12-08 22:44:51 +0600

    model project done

--- project_r06/first_app/models.py
+++ project_r06/first_app/models.py
@@ -7,3 +7,9 @@ class Student(models.Model):
     name = models.CharField(max_length= 20)
     roll = models.IntegerField(primary_key= True)
     address = models.TextField()
+    father_name = models.TextField(default="Ratul")
+    # blank = True
+    # null = True
+
+    def __str__(self):
+        return f"Roll: {self.roll} - {self.name}"
--- project_r06/first_app/urls.py
+++ project_r06/first_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name= "homepage"),
+    path('delete/<int:roll>', views.delete_spouse, name= "delete_spouse"),
 ]
